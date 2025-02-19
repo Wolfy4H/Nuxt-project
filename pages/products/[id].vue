5379e77290931334df3c520341932049f8b3f31b
Wolfy4H <Leon.Kersten@outlook.com>
2025-02-19 10:54:57 +0100

    id route added

---
+++ pages/products/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <p>Product details for {{ id }} </p>
+        <p>lorem ipsum</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
